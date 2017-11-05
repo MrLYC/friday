bc6f94982753483e3584b30a757b723123d3cd0f
MrLYC <imyikong@gmail.com>
2017-11-05 22:12:22 +0800

    add GetGlobal

--- vm/vm.go
+++ vm/vm.go
@@ -8,10 +8,32 @@ type VM struct {
 	LuaState *lua.LState
 }
 
+func NewVM() *VM {
+	vm := &VM{}
+	vm.Init()
+	return vm
+}
+
 func (v *VM) Init() {
 	v.LuaState = lua.NewState()
 }
 
 func (v *VM) Execute(statement string) error {
 	return v.LuaState.DoString(statement)
+}
+
+func (v *VM) GetGlobal(name string) interface{} {
+	value := v.LuaState.GetGlobal(name)
+	switch value.Type() {
+	case lua.LTNil:
+		return nil
+	case lua.LTBool:
+		return bool(value.(lua.LBool))
+	case lua.LTNumber:
+		return float64(value.(lua.LNumber))
+	case lua.LTString:
+		return string(value.(lua.LString))
+	default:
+		return value
+	}
 }
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -1,12 +1,31 @@
 package vm_test
 
 import (
+	"fmt"
 	"friday/vm"
 	"testing"
 )
 
 func TestExecute(t *testing.T) {
-	vm := vm.VM{}
-	vm.Init()
-	vm.Execute(`print("hello world")`)
+	v := vm.VM{}
+	v.Init()
+	v.Execute(`print("hello world")`)
+}
+
+func TestGetGlobal(t *testing.T) {
+	v := vm.NewVM()
+	items := map[string]interface{}{
+		"null":  nil,
+		"true":  true,
+		"false": false,
+		"1":     1.0,
+		"2.2":   2.2,
+		`"3"`:   "3",
+	}
+	for statement, value := range items {
+		v.Execute(fmt.Sprintf("value = %s", statement))
+		if v.GetGlobal("value") != value {
+			t.Errorf("get global error")
+		}
+	}
 }
