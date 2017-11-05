aec6d7fde271ea96e7ab6c5a7280bec16dc0e102
MrLYC <imyikong@gmail.com>
2017-11-06 00:41:30 +0800

    add table each functions

--- vm/vm.go
+++ vm/vm.go
@@ -4,6 +4,30 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
+type Table map[interface{}]interface{}
+
+type TableEachFunc func(key interface{}, value interface{})
+
+func (t Table) EachAsArray(f TableEachFunc) {
+	lenTable := len(t)
+	for index := 1; index <= lenTable; index++ {
+		value, ok := t[float64(index)]
+		if !ok {
+			break
+		}
+		f(index-1, value)
+	}
+}
+
+func (t Table) EachAsStrMap(f TableEachFunc) {
+	for key, value := range t {
+		switch key.(type) {
+		case string:
+			f(key.(string), value)
+		}
+	}
+}
+
 type VM struct {
 	LuaState *lua.LState
 }
@@ -14,16 +38,18 @@ func NewVM() *VM {
 	return vm
 }
 
+// Init :
 func (v *VM) Init() {
 	v.LuaState = lua.NewState()
 }
 
+// Execute :
 func (v *VM) Execute(statement string) error {
 	return v.LuaState.DoString(statement)
 }
 
-func (v *VM) GetGlobal(name string) interface{} {
-	value := v.LuaState.GetGlobal(name)
+// ParseValue :
+func (v *VM) ParseValue(value lua.LValue) interface{} {
 	switch value.Type() {
 	case lua.LTNil:
 		return nil
@@ -33,7 +59,21 @@ func (v *VM) GetGlobal(name string) interface{} {
 		return float64(value.(lua.LNumber))
 	case lua.LTString:
 		return string(value.(lua.LString))
+	case lua.LTTable:
+		table := Table{}
+		value.(*lua.LTable).ForEach(func(lkey lua.LValue, litem lua.LValue) {
+			key := v.ParseValue(lkey)
+			item := v.ParseValue(litem)
+			table[key] = item
+		})
+		return table
 	default:
 		return value
 	}
+}
+
+// GetGlobal :
+func (v *VM) GetGlobal(name string) interface{} {
+	value := v.LuaState.GetGlobal(name)
+	return v.ParseValue(value)
 }
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -29,3 +29,64 @@ func TestGetGlobal(t *testing.T) {
 		}
 	}
 }
+
+func TestTableEachAsArray(t *testing.T) {
+	var (
+		v   = vm.NewVM()
+		arr = make([]float64, 0, 3)
+	)
+
+	v.Execute(`value = {1, 2, 3}`)
+	value := v.GetGlobal("value").(vm.Table)
+
+	value.EachAsArray(func(index interface{}, value interface{}) {
+		arr = append(arr, value.(float64))
+	})
+
+	if arr[0] != 1 || arr[1] != 2 || arr[2] != 3 {
+		t.Errorf("array error: %v", arr)
+	}
+}
+
+func TestTableEachAsStrMap(t *testing.T) {
+	var (
+		v    = vm.NewVM()
+		dict = make(map[string]float64)
+	)
+
+	v.Execute(`value = {a=1, b=2, c=3}`)
+	value := v.GetGlobal("value").(vm.Table)
+
+	value.EachAsStrMap(func(key interface{}, value interface{}) {
+		dict[key.(string)] = value.(float64)
+	})
+
+	if dict["a"] != 1 || dict["b"] != 2 || dict["c"] != 3 {
+		t.Errorf("map error: %v", dict)
+	}
+}
+
+func TestTableMix(t *testing.T) {
+	var (
+		v    = vm.NewVM()
+		dict = make(map[string]float64)
+		arr  = make([]float64, 0, 2)
+	)
+
+	v.Execute(`value = {a=1, 2, b=3, 4}`)
+	value := v.GetGlobal("value").(vm.Table)
+
+	value.EachAsStrMap(func(key interface{}, value interface{}) {
+		dict[key.(string)] = value.(float64)
+	})
+	if dict["a"] != 1 || dict["b"] != 3 {
+		t.Errorf("map error: %v", dict)
+	}
+
+	value.EachAsArray(func(index interface{}, value interface{}) {
+		arr = append(arr, value.(float64))
+	})
+	if arr[0] != 2 || arr[1] != 4 {
+		t.Errorf("array error: %v", arr)
+	}
+}
