85878be7aa1f6e92b93d4c03b294331238aaeb02
MrLYC <imyikong@gmail.com>
2018-02-04 22:39:21 +0800

    add tableitem

--- storage/cache/memcache/tableitem.go
+++ storage/cache/memcache/tableitem.go
@@ -1,9 +1,122 @@
 package memcache
 
+import (
+	"errors"
+
+	"github.com/emirpasic/gods/maps/hashmap"
+)
+
 // TypeMappingTableItem :
 const TypeMappingTableItem = "table"
+
+//
+var (
+	ErrTableFieldValueError = errors.New("Table field value error")
+)
 
 // MappingTableItem :
 type MappingTableItem struct {
 	ComplexMappingItem
+}
+
+// Init :
+func (i *MappingTableItem) Init() {
+	i.Value = hashmap.New()
+}
+
+// GetTable :
+func (i *MappingTableItem) GetTable() *hashmap.Map {
+	if i.Value == nil {
+		return nil
+	}
+	return i.Value.(*hashmap.Map)
+}
+
+// Length :
+func (i *MappingTableItem) Length() int {
+	table := i.GetTable()
+	if table == nil {
+		return 0
+	}
+	return table.Size()
+}
+
+// GetString :
+func (i *MappingTableItem) GetString(field string) string {
+	table := i.GetTable()
+	if table == nil {
+		return ""
+	}
+	value, ok := table.Get(field)
+	if !ok {
+		return ""
+	}
+	return value.(string)
+}
+
+// SetString :
+func (i *MappingTableItem) SetString(field string, value string) error {
+	table := i.GetTable()
+	if table == nil {
+		return ErrTableFieldValueError
+	}
+	table.Put(field, value)
+	return nil
+}
+
+// Delete :
+func (i *MappingTableItem) Delete(field string) error {
+	table := i.GetTable()
+	if table == nil {
+		return ErrTableFieldValueError
+	}
+	table.Remove(field)
+	return nil
+}
+
+// Clear :
+func (i *MappingTableItem) Clear() error {
+	table := i.GetTable()
+	if table == nil {
+		return ErrTableFieldValueError
+	}
+	table.Clear()
+	return nil
+}
+
+// Exists :
+func (i *MappingTableItem) Exists(field string) bool {
+	table := i.GetTable()
+	if table == nil {
+		return false
+	}
+	_, ok := table.Get(field)
+	return ok
+}
+
+// GetMappings :
+func (i *MappingTableItem) GetMappings() map[string]string {
+	result := make(map[string]string, i.Length())
+	table := i.GetTable()
+	if table != nil {
+		for _, field := range table.Keys() {
+			value, ok := table.Get(field)
+			if ok {
+				result[field.(string)] = value.(string)
+			}
+		}
+	}
+	return result
+}
+
+// SetMappings :
+func (i *MappingTableItem) SetMappings(mappings map[string]string) error {
+	table := i.GetTable()
+	if table == nil {
+		return ErrTableFieldValueError
+	}
+	for field, value := range mappings {
+		table.Put(field, value)
+	}
+	return nil
 }
---
+++ storage/cache/memcache/tableitem_test.go
@@ -0,0 +1,89 @@
+package memcache_test
+
+import (
+	"testing"
+
+	"friday/storage/cache/memcache"
+)
+
+func TestMappingTableItem1(t *testing.T) {
+	var item memcache.MappingTableItem
+	item.Init()
+
+	if item.SetString("a", "1") != nil {
+		t.Errorf("set string failed")
+	}
+	if item.SetString("b", "2") != nil {
+		t.Errorf("set string failed")
+	}
+	if item.Length() != 2 {
+		t.Errorf("mapping length error")
+	}
+
+	if !item.Exists("a") {
+		t.Errorf("exists error")
+	}
+	if !item.Exists("b") {
+		t.Errorf("exists error")
+	}
+	if item.Exists("c") {
+		t.Errorf("exists error")
+	}
+
+	if item.GetString("a") != "1" {
+		t.Errorf("get string failed")
+	}
+	if item.GetString("b") != "2" {
+		t.Errorf("get string failed")
+	}
+	if item.GetString("c") != "" {
+		t.Errorf("get string failed")
+	}
+
+	if item.Delete("b") != nil {
+		t.Errorf("delete failed")
+	}
+	if item.Length() != 1 {
+		t.Errorf("mapping length error")
+	}
+	if item.Clear() != nil {
+		t.Errorf("clear failed")
+	}
+	if item.Length() != 0 {
+		t.Errorf("mapping length error")
+	}
+}
+
+func TestMappingTableItem2(t *testing.T) {
+	var item memcache.MappingTableItem
+	item.Init()
+
+	if item.SetMappings(map[string]string{
+		"a": "1",
+		"b": "2",
+	}) != nil {
+		t.Errorf("set mappings failed")
+	}
+
+	values := item.GetMappings()
+	if item.Length() != len(values) {
+		t.Errorf("mapping length error")
+	}
+	if values["a"] != "1" || values["b"] != "2" {
+		t.Errorf("get mappings error")
+	}
+
+	if item.SetMappings(map[string]string{
+		"b": "0",
+		"c": "3",
+	}) != nil {
+		t.Errorf("set mappings failed")
+	}
+	values = item.GetMappings()
+	if item.Length() != len(values) {
+		t.Errorf("mapping length error")
+	}
+	if values["a"] != "1" || values["b"] != "0" || values["c"] != "3" {
+		t.Errorf("get mappings error")
+	}
+}
