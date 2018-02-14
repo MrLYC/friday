3c323ced2030b3e5795df7c52eb568773f79524f
MrLYC <imyikong@gmail.com>
2018-02-15 00:52:29 +0800

    update migrate_test.sh

--- testdata/scripts/migrate_test.sh
+++ testdata/scripts/migrate_test.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-conf_file=/tmp/migrate_test.yaml
-dbname=/tmp/migrate_test.db
+function migrate() {
+  conf_file="$1"
+  ${TARGET} confinfo -c ${conf_file}
+  ${TARGET} migrate -c ${conf_file} -action rollback || exit $?
+  ${TARGET} migrate -c ${conf_file} || exit $?
+  ${TARGET} migrate -c ${conf_file} -action rebuild || exit $?
+  ${TARGET} migrate -c ${conf_file} -action rollback || exit $?
+  ${TARGET} migrate -c ${conf_file} -action list || exit $?
+}
 
-cat > ${conf_file} << EOF 
+function sqlite3() {
+  conf_file=/tmp/migrate_test.yaml
+  dbname=/tmp/migrate_test.db
+  cat > ${conf_file} << EOF 
 database:
   type: sqlite3
   name: ${dbname}
 EOF
 
-${TARGET} confinfo -c ${conf_file}
-${TARGET} migrate -c ${conf_file} -action rollback || exit $?
-${TARGET} migrate -c ${conf_file} || exit $?
-${TARGET} migrate -c ${conf_file} -action rebuild || exit $?
-${TARGET} migrate -c ${conf_file} -action rollback || exit $?
-${TARGET} migrate -c ${conf_file} -action list || exit $?
+  migrate "${conf_file}"
 
-rm -rf "${dbname}"
-rm -rf "${conf_file}"
+  rm -rf "${dbname}"
+  rm -rf "${conf_file}"
+}
+
+sqlite3
